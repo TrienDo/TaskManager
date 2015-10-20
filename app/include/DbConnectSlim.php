0be343dbd9bf133a08a21a5fbc3b25b02196c78e
TrienDo <trien.v.do@gmail.com>
2015-10-20 20:11:54 +0100

    Add missing files

---
+++ app/include/DbConnectSlim.php
@@ -0,0 +1,29 @@
+<?php
+	// Database information
+	$settings = array(
+		'driver' => 'mysql',
+		'host' => 'localhost',
+		'database' => 'sharc',
+		'username' => 'root',
+		'password' => 'root',
+		'charset'   => 'utf8',
+		'collation' => 'utf8_general_ci',
+		'prefix' => ''
+	);
+
+	// Bootstrap Eloquent ORM
+	/*$connFactory = new \Illuminate\Database\Connectors\ConnectionFactory();
+	$conn = $connFactory->make($settings);
+	$resolver = new \Illuminate\Database\ConnectionResolver();
+	$resolver->addConnection('default', $conn);
+	$resolver->setDefaultConnection('default');
+	\Illuminate\Database\Eloquent\Model::setConnectionResolver($resolver);*/
+	
+	use Illuminate\Database\Capsule\Manager as Capsule;  
+
+	$capsule = new Capsule; 
+
+	$capsule->addConnection($settings);
+
+	$capsule->bootEloquent();
+?>
---
+++ v1/indexBook.php
@@ -0,0 +1,28 @@
+<?php	
+	//require 'vendor/slim/slim/Slim/Slim.php';	
+	require ("../vendor/autoload.php");
+	require ("../app/models/Book.php");		
+	require ("../app/include/DbConnectSlim.php");
+	
+	\Slim\Slim::registerAutoloader();
+	$app = new \Slim\Slim();
+	$app->get('/hello/:name', function ($name) {
+		echo "Hello, " . $name;
+	});
+	
+	$app->get('/books', function () {
+		// Fetch all books
+		$books = Book::all();
+		echo $books->toJson();
+
+		/* Or create a new book
+		$book = new \Book(array(
+			'title' => 'C#',
+			'author' => 'Microsoft'
+		));
+		$book->save();
+		echo $book->toJson();
+		*/
+	});
+	$app->run();
+?>
